d2ee0747e1c98da571609b635ded7192be6bdae9
kc merrill <kcmerrill@gmail.com>
2015-03-13 10:46:53 -0600

    Update yoda.php

--- src/kcmerrill/yoda.php
+++ src/kcmerrill/yoda.php
@@ -86,7 +86,7 @@ class yoda {
                 if(is_file('.yoda')) {
                     file_put_contents($new_share, file_get_contents('.yoda'));
                     $this->app['cli']->out('<green>[Do]</green> <white>' . $share_as . '</white>');
-                    $this->app['cli']->out('<green>Shared your wisdom with the world, I have.  Hmmmmmm.</green>');
+                    $this->app['cli']->out('<green>[Yoda]</green> <white>Shared your wisdom with the world, I have.  Hmmmmmm.</white>');
                 } else {
                     throw new \Exception('Have, a valid .yoda file I must.');
                 }
